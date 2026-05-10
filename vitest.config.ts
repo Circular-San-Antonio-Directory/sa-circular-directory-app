903f09be4d0ca4e5327468bdb8f45fbad38d1243
gabrielkilly <gabykilly@gmail.com>
2026-05-10 22:31:46 +0000

    add listing details tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,20 @@ import { defineConfig } from 'vitest/config';
 import path from 'path';
 
 export default defineConfig({
+  plugins: [
+    {
+      name: 'mock-scss-modules',
+      transform(_code, id) {
+        if (id.endsWith('.module.scss')) {
+          return { code: 'export default new Proxy({}, { get: (_, k) => String(k) })' };
+        }
+      },
+    },
+  ],
   test: {
+    globals: true,
     environment: 'node',
+    setupFiles: ['./src/test-setup.ts'],
   },
   resolve: {
     alias: {
